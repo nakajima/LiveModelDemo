34cb52bff23ef5043bf768fb4fd66d829102fac5
Pat Nakajima <patnakajima@gmail.com>
2024-05-05 14:20:06 -0700

    update this

--- LiveModelDemo/LiveModelDemoApp.swift
+++ LiveModelDemo/LiveModelDemoApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct LiveModelDemoApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+            Person.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
